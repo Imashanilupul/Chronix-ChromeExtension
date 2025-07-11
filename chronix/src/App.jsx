3620703b4912d7eda5ebe197002611fbb49e43b8
Imasha Nilupul <imashanilupul@gmail.com>
2025-07-11 23:03:06 +0530

    Some minor changes

--- chronix/src/App.jsx
+++ chronix/src/App.jsx
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Graphs from "./pages/Graphs";
 import Popup from "./pages/popup";
+import Settings from "./pages/Settings";
 
 
 
@@ -13,8 +14,9 @@ export default function App() {
         <Route path="/" element={<Popup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/graphs" element={<Graphs />} />
-            
-       
+        <Route path="/settings" element={<Settings />} />
+
+
       </Routes>
     </HashRouter>
   );
